4997268b12555b29e04cf00842634c9b43038bc7
rownak <rownak@example.com>
2024-03-05 12:03:19 +0600

    test

---
+++ src/javaClassStructure/com/example/Main.java
@@ -0,0 +1,11 @@
+package javaClassStructure.com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+        for (String s: args){
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/javaClassStructure/com/example/Person.java
@@ -0,0 +1,14 @@
+package javaClassStructure.com.example;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void getFirstName(String firstName){
+        this.firstName = firstName;
+    }
+}
