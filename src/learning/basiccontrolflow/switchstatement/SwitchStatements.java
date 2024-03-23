5e7886beaa3549081f3f5679c8b5996a7dde1778
rownak <rownak@example.com>
2024-03-23 17:06:07 +0600

    basic control flow

--- src/learning/basiccontrolflow/switchstatement/SwitchStatements.java
+++ src/learning/basiccontrolflow/switchstatement/SwitchStatements.java
@@ -1,17 +1,30 @@
 package learning.basiccontrolflow.switchstatement;
 
+import java.util.Random;
+
 public class SwitchStatements {
     public static void main(String[] args) {
-        int day = 0;
+        int day = 1;
         switch (day) {
             case 1:
                 System.out.println("Saturday");
-                break;
+                //break;
             case 2:
                 System.out.println("Sunday");
-                break;
+                break; //will print till this
+            case 3:
+                System.out.println("Monday");
             default:
                 System.out.println("wrong day");
+        }
+        Random random = new Random();
+        System.out.println(random);
+        int x  = random.nextInt(5) + 'a';
+        switch (x){
+            case 'e':
+            case 'c':
+                System.out.println("test");
+                break;
         }
     }
 }
